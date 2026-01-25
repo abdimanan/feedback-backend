e1730d2061bb691dee4cb7564884ade9bc5bea22
Abdimajid Abdirahman Ibrahim <54802395+abdimajid123@users.noreply.github.com>
2026-01-25 14:39:18 +0300

    done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureRole::class,
         ]);
+
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
